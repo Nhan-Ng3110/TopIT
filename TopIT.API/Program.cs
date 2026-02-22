a742d771d3ad807261710493fc44f48a5e88eb61
Nguyễn Thành Nhân <tnhan31102005@gmail.com>
2026-02-22 09:50:35 +0700

    Khoi tao Angular UI cho TopIT

--- TopIT.API/Program.cs
+++ TopIT.API/Program.cs
@@ -1,27 +1,35 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using TopIT.Infrastructure.Data;
 using TopIT.Core.Interfaces;
-
 using TopIT.Infrastructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// ---  CẤU HÌNH CORS  ---
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngularApp",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:4200") 
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+});
+
 builder.Services.AddScoped<IJobRepository, JobRepository>();
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
-// Đăng ký AppDbContext sử dụng SQL Server
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(connectionString));
-// Add services to the container.
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
@@ -29,10 +37,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+// --- 2. KÍCH HOẠT CORS (Thêm dòng này trước UseAuthorization) ---
+app.UseCors("AllowAngularApp");
+
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
